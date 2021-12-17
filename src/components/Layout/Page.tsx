1f079b6a442332d527dab02973bfd8e70044d129
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2021-12-16 19:32:46 -0800

    Page component: remove min height

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -8,7 +8,6 @@ import { useCakeBusdPrice } from 'hooks/useBUSDPrice'
 import Container from './Container'
 
 const StyledPage = styled(Container)`
-  min-height: calc(100vh - 64px);
   padding-top: 16px;
   padding-bottom: 16px;
 
