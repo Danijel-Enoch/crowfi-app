7dabd6132911a67137ba0d8efbbe2fa8c5a49484
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2021-12-18 18:34:50 -0800

    update testnet farms

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -13,7 +13,7 @@ const farms: SerializedFarmConfig[] = [
     lpAddresses: {
       97: '0xe9412a9809FadBbaCd8D1bd024E6280f05Bd2437',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      338: '0x360402fEC9663d19A60646E057129AD71722F173',
+      338: '0xe59179D3Dd34C1d33Cb938e29618aa9B11B4A073',
       25: '0x360402fEC9663d19A60646E057129AD71722F173',
     },
     token: serializedTokens.usdc,
@@ -25,7 +25,7 @@ const farms: SerializedFarmConfig[] = [
     lpAddresses: {
       97: '0xe890519b297700BB69a62F18AaA50cAc201A300C',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
-      338: '0x50FEEEc566A9DFF6E012A970934b9b5B296F2191',
+      338: '0xEf5b46555a8548b3E7CaCcB8511926643c8e7Ba2',
       25: '0x50FEEEc566A9DFF6E012A970934b9b5B296F2191'
     },
     token: serializedTokens.crow,
@@ -37,7 +37,7 @@ const farms: SerializedFarmConfig[] = [
     lpAddresses: {
       97: '',
       56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
-      338: '0xfdc06C084f62109fe23566cf26a2c8aCe9a7Aa90',
+      338: '0xed55E937966d1B2190Ce9A8D7B27619c1996EFAb',
       25: '0xfdc06C084f62109fe23566cf26a2c8aCe9a7Aa90',
     },
     token: serializedTokens.usdc,
