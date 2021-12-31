ca3bfceeb42a26d7831b98b177217356eb24331d
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2021-12-30 23:44:41 -0800

    spelling

--- src/views/Home/components/TradeSection.tsx
+++ src/views/Home/components/TradeSection.tsx
@@ -25,7 +25,7 @@ const TradeSection = () => {
             {t('Real Trading Tools for Real Traders')}
             </LandingHeading>
             <FuturaText scale="md" textAlign="center" color="primary"  mb="24px">
-            {t('(Comming Soon)')}
+            {t('(Coming Soon)')}
             </FuturaText>
             <FuturaText scale="lg" textAlign="center" color="primary">
             {t('CrowFi provides a secure platform to trade with high leverage and low fees. Introducing Stop Losses and Limit Orders enables efficient trading at safe price ranges!')}
