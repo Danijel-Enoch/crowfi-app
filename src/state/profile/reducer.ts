2eadd5a0b2c75c3a67c667f4a0d22dc3d822a736
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-04-21 19:22:15 -0700

    nft: fix login issue

--- src/state/profile/reducer.ts
+++ src/state/profile/reducer.ts
@@ -28,25 +28,29 @@ export default createReducer(initialState, (builder) =>
       state.tokenData = action.payload.tokenData
     })
     .addCase(updateProfileComplete, (state, action) => {
-      state.profile.name = action.payload.profile.name
-      state.profile.address = action.payload.profile.address
-      state.profile.banner = action.payload.profile.banner
-      state.profile.portfolio = action.payload.profile.portfolio
-      state.profile.createdAt = action.payload.profile.createdAt
+      state.profile = {
+        ...state.profile,
+        ...action.payload.profile
+      }
     })
     .addCase(updateProfile, (state, action) => {
+      const data:ProfilePublicData = {}
       if (action.payload.profile.name) {
-        state.profile.name = action.payload.profile.name
+        data.name = action.payload.profile.name
       }
       if (action.payload.profile.address) {
-        state.profile.address = action.payload.profile.address
+        data.address = action.payload.profile.address
       }
       if (action.payload.profile.banner) {
-        state.profile.banner = action.payload.profile.banner
+        data.banner = action.payload.profile.banner
       }
       if (action.payload.profile.createdAt) {
-        state.profile.portfolio = action.payload.profile.portfolio
-        state.profile.createdAt = action.payload.profile.createdAt
+        data.portfolio = action.payload.profile.portfolio
+        data.createdAt = action.payload.profile.createdAt
+      }
+      state.profile = {
+        ...state.profile,
+        ...data
       }
     })
 )
